ddaa8cb4ac31ad3317650cce0128afe829b36d44
Chris Dzombak <chris@chrisdzombak.net>
2012-01-30 23:19:57 -0500

    Improve loading UITableViewCell appearance

---
+++ CAENLabStatus/DZCAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  DZCAboutViewController.h
+//  CAENLabStatus
+//
+//  Created by Chris Dzombak on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZCAboutViewController : UIViewController
+
+@end
